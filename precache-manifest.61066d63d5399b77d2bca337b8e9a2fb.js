4e40601a38909a605316597be927b367c063be41
bori8011 <borigo8011@gmail.com>
2024-12-06 20:48:43 +0900

    Updates

---
+++ precache-manifest.61066d63d5399b77d2bca337b8e9a2fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec7e3199707e401362f82b6f076ac47",
+    "url": "/bungieapp/index.html"
+  },
+  {
+    "revision": "44d4967ff27c800c0b71",
+    "url": "/bungieapp/static/css/main.a8750211.chunk.css"
+  },
+  {
+    "revision": "ed1a2798e56f41e2f92a",
+    "url": "/bungieapp/static/js/2.8624f54b.chunk.js"
+  },
+  {
+    "revision": "206e125ed3b255cc8f43a8fc00b4a959",
+    "url": "/bungieapp/static/js/2.8624f54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2e67853b9e170f0480",
+    "url": "/bungieapp/static/js/3.15856599.chunk.js"
+  },
+  {
+    "revision": "44d4967ff27c800c0b71",
+    "url": "/bungieapp/static/js/main.0dd223ae.chunk.js"
+  },
+  {
+    "revision": "65372c746cbec162e9c5",
+    "url": "/bungieapp/static/js/runtime-main.d1bb0cd7.js"
+  }
+]);
